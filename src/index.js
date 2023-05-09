4ec5a3b8899886af0205195e4030b03663a350f5
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-05-09 12:33:13 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', (event) => {
+	// Select the form and input elements
+	let form = document.getElementById('create-task-form');
+	let input = document.getElementById('new-task-description');
+	let list = document.getElementById('list');
+
+	// Listen for the form's submit event
+	form.addEventListener('submit', function (event) {
+		// Prevent the form from being submitted
+		event.preventDefault();
+
+		// Create a new list item
+		let newTask = document.createElement('li');
+
+		// Set the text content of the new item to the value of the input
+		newTask.textContent = input.value;
+
+		// Append the new item to the list
+		list.appendChild(newTask);
+
+		// Clear the input
+		input.value = '';
+	});
 });
